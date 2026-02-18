a2cd759b4c2bc45e909f9112f06e6b75d45b0b89
Tapesh Chavle <145046947+tapeshchavle@users.noreply.github.com>
2026-02-18 23:39:22 +0530

    Update CorsConfig.java

--- src/main/java/com/food/config/CorsConfig.java
+++ src/main/java/com/food/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // or "/api/**" if you're only using that path
-                .allowedOrigins("https://foodingo.netlify.app", "https://foodingo.tapesh.me","https://admin-foodingo.tapesh.me")
+                .allowedOrigins("https://foodingo.netlify.app", "https://foodingo.tapesh.me","https://admin-foodingo.tapesh.me","https://app.foodingo.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
