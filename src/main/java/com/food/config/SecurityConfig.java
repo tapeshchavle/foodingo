76e6e5a461a698d3055a5f6e99333309e9104ac6
tapeshchavle <tapeshchawle@gmail.com>
2026-04-23 01:45:54 +0530

    add home controller

--- src/main/java/com/food/config/SecurityConfig.java
+++ src/main/java/com/food/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
 				.csrf(AbstractHttpConfigurer::disable)
 				.authorizeHttpRequests(auth -> auth
 						.requestMatchers("/api/user/register", "/api/user/login", "api/orders/status/**",
-								"/api/foods/**", "/api/orders/all")
+								"/api/foods/**", "/api/orders/all","/")
 						.permitAll()
 						.anyRequest()
 						.authenticated())
